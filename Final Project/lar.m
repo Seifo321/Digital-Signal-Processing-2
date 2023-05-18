a1141acd7eb01a6f0b771cbc68d19bfab040bc66
M0hammed-Eid <eng.mohammedeid2024@gmail.com>
2023-05-19 02:28:02 +0300

    update lar.m

--- Final Project/lar.m
+++ Final Project/lar.m
@@ -4,12 +4,12 @@ function LarCoefficients = lar(lpc_coefficients)
     for i = 1 : length(lpc_coefficients)
         if lpc_coefficients{i, 1}.voiced == 0
 			lar_short = poly2rc(lpc_coefficients{i, 1}.short);
-            LarCoefficients{i} = struct('short',lar_short);
+            LarCoefficients{i} = struct('voiced', false,'short',lar_short);
 
 		else
 			lar_short = poly2rc(lpc_coefficients{i, 1}.short);
 			lar_long = poly2rc(lpc_coefficients{i, 1}.long);
-            LarCoefficients{i} = struct('long',lar_long ,...
+            LarCoefficients{i} = struct('voiced', true,'long',lar_long ,...
                 'short',lar_short);
         end
             
